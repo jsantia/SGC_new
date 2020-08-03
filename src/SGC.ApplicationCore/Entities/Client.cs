2cf38caa0c11f3ebeac5ce98efb70f17e4ae5529
jsantia <jsantia@outlook.com>
2020-08-03 15:29:09 +0100

    Add project files.

---
+++ src/SGC.ApplicationCore/Entities/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entities
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime date_of_birth { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+
+    }
+}
